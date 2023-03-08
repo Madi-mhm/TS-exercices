7eab038197846743a00290a61675efbf7d9db851
Mahdi <you@example.comcls>
2023-03-08 11:51:12 +0100

    exercice js done

--- demo.ts
+++ demo.ts
@@ -1,20 +1,40 @@
-// // Système de constante et variable
-// // Différent types
+// Exercice 0  => Done
 
-// const firstName: string = 'Kévin';
-// const lastName: string = 'Wolff'; 
+// Exercice 1  
+const address: string = "FBG Montmélian";
+const zipcode: number = 73000;
 
-// const age: number = 30
+function myAddress(){
+    const addressFull: string = address + ", " + zipcode; 
+    console.log(addressFull);
+}
+myAddress();
 
-// const isHeWakeUp: boolean = false;
 
-// // Tableau
-// const learners: any[] = ['Kevin', 30, true]
+// Exercice 2
+const myFavoriteContries: string[] = ['USA', 'France', 'Sweden', 'Iran', 'Japon']; 
 
-// // Objet
-// const learner: any = {
-//     name: 'Kevin',
-//     age: 30,
-//     wakeUp: false
-// }
+function contries() {
+    for( let i = 0 ; i < myFavoriteContries.length ; i++){
+        console.log(myFavoriteContries[i]);
+    };
+};
+contries();
 
+
+// Exercice 3 
+const myName: string= "Mahdi";
+
+function magic(){
+    console.log(myName.split(""));
+};
+magic();
+
+
+// Exercice 4
+const nameDecomposee: string[] = ["M", "a", "h", "d", "i"];
+function fullName (){
+    const resultat: string = nameDecomposee.join("");
+    console.log(resultat);
+} 
+fullName();

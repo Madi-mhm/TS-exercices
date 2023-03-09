74d0bbee61cf7cff7364037bd9e68d4abe14fc20
Mahdi <you@example.comcls>
2023-03-09 15:12:44 +0100

    exercice-3 is done

--- realisations/exercice2.ts
+++ realisations/exercice2.ts
@@ -1,37 +1,37 @@
-// Exercice 0  => Done
+// // Exercice 0  => Done
 
-// Exercice 1  
-function myAddress(){
-    const address: string = "FBG Montmélian";
-    const zipcode: number = 73000;
-    const addressFull: string = address + ", " + zipcode; 
-    console.log(addressFull);
-}
-myAddress();
-
-
-// Exercice 2
-function contries() {
-    const myFavoriteContries: string[] = ['USA', 'France', 'Sweden', 'Iran', 'Japon']; 
-    for( let i = 0 ; i < myFavoriteContries.length ; i++){
-        console.log(myFavoriteContries[i]);
-    };
-};
-contries();
+// // Exercice 1  
+// function myAddress(){
+//     const address: string = "FBG Montmélian";
+//     const zipcode: number = 73000;
+//     const addressFull: string = address + ", " + zipcode; 
+//     console.log(addressFull);
+// }
+// myAddress();
 
 
-// Exercice 3 
-function magic(){
-    const myName: string= "Mahdi";
-    console.log(myName.split(""));
-};
-magic();
+// // Exercice 2
+// function contries() {
+//     const myFavoriteContries: string[] = ['USA', 'France', 'Sweden', 'Iran', 'Japon']; 
+//     for( let i = 0 ; i < myFavoriteContries.length ; i++){
+//         console.log(myFavoriteContries[i]);
+//     };
+// };
+// contries();
 
 
-// Exercice 4
-const nameDecomposee: string[] = ["M", "a", "h", "d", "i"];
-function fullName (){
-    const resultat: string = nameDecomposee.join("");
-    console.log(resultat);
-} 
-fullName();
+// // Exercice 3 
+// function magic(){
+//     const myName: string= "Mahdi";
+//     console.log(myName.split(""));
+// };
+// magic();
+
+
+// // Exercice 4
+// const nameDecomposee: string[] = ["M", "a", "h", "d", "i"];
+// function fullName (){
+//     const resultat: string = nameDecomposee.join("");
+//     console.log(resultat);
+// } 
+// fullName();
---
+++ realisations/exercice3.ts
@@ -0,0 +1,58 @@
+// Exercice # 1
+// const data: string[] = ['Théo', 'Martin', 'Lucas', 'Antoine', 'Etienne']
+//     for(let i = 0 ; i < data.length ; i++){
+//         if(data[i].length <= 5){
+//             console.log(data[i])
+//         }
+//     }
+
+
+// // Exercice # 2
+// const countries: string[]= ["France", "Sweden", "Canada", "Japon", "UK"]
+// let sentence: string= ""
+// for(let i = 0 ; i < countries.length ; i++){
+//    let sentence2 = countries[i].toString()
+//    sentence = sentence + ", " + sentence2
+//    console.log(sentence)
+// }
+
+
+// // Exercice # 3
+// const numbers: number[] = [123, 999, 340, 12390]
+// let resu: number[] = []
+// for(let i = 0; i < numbers.length ; i++){
+//     resu.push(numbers[i] * 180)
+//     // console.log(resu)
+// }
+
+
+// Exercice # 4
+// const contante: object = { name: 'Marchal', surname: 'Mickeal', age: 19 }  
+//    if(contante['age'] >= 18){
+//     console.log("Verified!")
+//    }else{
+//     console.log("you're not allowed")
+//    }
+
+
+// Exercice # 5
+// const persoInfo: object = {nom : 'Chan', prénome : 'Jacky', age : '50'}  
+// console.log(`Bonjour je m'apple ${persoInfo['prénome']} ${persoInfo['nom']} et j'ai ${persoInfo['age']} ans `)
+
+
+// Exercice # 6
+// const users: object[]= [
+// {nom : 'Chan1', prénome : 'Jacky1', age : '51'}, 
+// {nom : 'chan2', prénome : 'Jacky2', age : '52'},
+// {nom : 'Chan3', prénome : 'Jacky3', age : '53'},
+// {nom : 'Chan4', prénome : 'Jacky4', age : '54'},
+// {nom : 'Chan5', prénome : 'Jacky5', age : '55'}  ]
+
+
+// for(let i = 0 ; i < users.length ; i++){
+//     console.log(`Bonjour je m'apple ${users[i]['prénome']} ${users[i]['nom']} et j'ai ${users[i]['age']} ans `)
+// }
+
+
+// Exercice # 7
+// Pushed in github
